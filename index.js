6afd02a357fa84ed7f1716bcc4ebb7a4591719ae
karen chehade <karen_chehade@hotmail.com>
2021-04-28 08:15:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ function saturdayFun(activity="roller-skate") {
 }
 saturdayFun();
 saturdayFun("dance");
+
+function mondayWork(activity='go to the office'){
+  return `This Monday, I will ${activity} .`;
+}
+
+mondayWork();
+mondayWork('go to the beach');
